6da80895edf8d95d82ed5b617520faff7bc2c9d5
Jake Palmer <jake.michael96@gmail.com>
2017-12-15 17:01:01 -0800

    First Commit

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+	//public GameObject PlayerBlock = GameObject.FindGameObjectsWithTag ("PlayerBlock");
+
+
+
+	public static GameLogic instance;         //A reference to our game control script so we can access it statically.
+
+
+	//GameObject playerObject;
+	public GameObject PlayerCube;
+	//Rigidbody2D rb2d;
+
+	public GameObject[] player;
+	public GameObject[] prevBlocks;
+	List<float> prevBlocksX = new List<float>();
+	List<float> tempList = new List<float> ();
+
+	void Awake()
+	{
+		//If we don't currently have a game control...
+		if (instance == null)
+			//...set this one to be it...
+			instance = this;
+		//...otherwise...
+		else if(instance != this)
+			//...destroy this one because it is a duplicate.
+			Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		int playerSize = 4;
+		player = new GameObject[playerSize];
+
+		for(int i =0; i < 9; i++){
+			prevBlocksX.Add (i);
+		}
+
+		//Creates Player Array
+		for (int i = 0; i < playerSize; i++) {
+			GameObject playerObject = Instantiate(PlayerCube, new Vector2(i, 1.5f), Quaternion.identity);
+
+			playerObject.AddComponent<PlayerScript>();
+			player [i] = playerObject;
+
+		}
+
+		//Creates PrevBlocks
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			for (int i = 0; i < player.Length; i++) {
+				if ((player [i].transform.position.x == prevBlocksX [0]) || (player [i].transform.position.x == prevBlocksX [1]) ||
+					(player [i].transform.position.x == prevBlocksX [2]) || (player [i].transform.position.x == prevBlocksX [3])) {
+
+					tempList.Add (player [i].transform.position.x);
+
+				} else {
+					player [i].SetActive (false);
+					Destroy (player [i]);
+					//Destroy (player[i].GetComponent(PlayerScript));
+
+				}
+
+
+			}
+
+			prevBlocksX = tempList;
+
+			for (int i = 0; i < tempList.Count; i++) {
+				GameObject playerClone = Instantiate (PlayerCube, new Vector2 (tempList [i], tempList [i]), Quaternion.identity);
+
+
+			}
+			tempList.Clear ();
+		}
+
+
+	}
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+
+	public GameObject PlayerCube;
+	public GameObject PlayerObject;
+	Rigidbody2D rb2d;
+	bool right = true;
+	public int count;
+	public bool grounded;
+	//public GameObject[] prevBlocks;
+	//List<float> prevBlocksX = new List<float>();
+
+	//double moveSpeed = 1.0;
+
+	// Use this for initialization
+	void Start () {
+
+		grounded = true;
+
+		PlayerCube  = GameObject.Find("PlayerBlock");
+		//PlayerCube = this.gameObject;  //GameObject with script?
+
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		count = 1;
+
+		InvokeRepeating ("movement", 3.0f, 0.7f);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+		//PlayerInput
+		if (Input.GetKeyDown(KeyCode.Space)){
+			//GameLogic.instance.prevBlocks.CopyTo
+			//grounded = checkGrounded ();
+
+			//placeCube ();
+			rb2d.transform.Translate (0, 1, 0);
+			//} else {
+			//missedBlock();
+			//this.gameObject.SetActive(false);
+			//-or-
+			//Destroy(this.gameObject);
+		}
+
+		//			prevBlocksX = tempList;
+		//				for (int i = 0; i < 4; i++) {
+		//				tempList.Remove (i);
+		//}
+
+	}
+
+	public void movement(){
+
+		//Right Left boolean
+		if (right == true) {
+			transform.Translate (1, 0, 0);
+			count++;
+		} else {
+			rb2d.transform.Translate (-1, 0, 0);
+			count--;
+		}
+
+
+
+
+		//Constraints
+		//if (rb2d.transform.position.x > 5) {
+		if(count > 3){
+			right = false;
+		} 
+		//else if (rb2d.transform.position.x < -5){
+		else if (count <-4){
+			right = true;
+		}
+
+	}
+
+	//	public void placeCube(){
+	//
+	//		PlayerObject = this.gameObject;
+	//		float cubeX = PlayerObject.transform.position.x;
+	//		float cubeY = PlayerObject.transform.position.y;
+	//
+	//	//Instantiation stuff
+	//		GameObject playerClone = Instantiate(PlayerCube, new Vector2(cubeX,cubeY), Quaternion.identity);
+	//		//playerClone.name = "blockClone";
+	//
+	//
+	//		//refrence prevBlocks and add X value
+	//		//prevBlocksX.Add(cubeX);
+	//
+	//	}
+
+	//	public bool checkGrounded(){
+	//		//for (int i = 0; i < 4; i++ ){
+	//		//for (this.gameObject.transform.position.x == int prevBlocksX){
+	//			
+	//		if ((this.gameObject.transform.position.x == prevBlocksX[0]) || (this.gameObject.transform.position.x == prevBlocksX[1]) || 
+	//			(this.gameObject.transform.position.x == prevBlocksX[2]) || (this.gameObject.transform.position.x == prevBlocksX[3])) {
+	//			return true;
+	//		} else {
+	//			return false;
+	//		}
+	//		
+	//	//}			
+	////
+	////	/* Transform for children objects
+	////	 * void Example() {
+	////        foreach (Transform child in transform) {
+	////            child.position += Vector3.up * 10.0F;
+	////        }
+	////	 * */
+	////}
+	//}
+}
